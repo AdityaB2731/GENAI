ecd1320aa22bbbf312c65e888eb5bcac5b84fa25
Aditya Bajaj <aditya3105@users.noreply.huggingface.co>
2026-01-16 10:30:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,60 @@ import streamlit as st
 import pickle
 import pandas as pd
 import tensorflow as tf
-# from tensorflow.keras.models import load_model
-from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
-import pickle
 
-model = tf.keras.models.load_model('model.h5')
+# Load model
+model = tf.keras.models.load_model("model.keras")
 
-with open('label_encoder_gender.pkl','wb') as file:
-    label_encoder_gender=pickle.load(file)
-with open('onehot_encoder_geo.pkl','wb') as file:
-    onehot_encoder_geo=pickle.load(file)
-with open('scaler.pkl','rb') as file:
-    scaler=pickel.load(file)
-st.title('customer churn prediction')
+# Load encoders & scaler
+with open("label_encoder_gender.pkl", "rb") as f:
+    label_encoder_gender = pickle.load(f)
 
+with open("onehot_encoder_geo.pkl", "rb") as f:
+    onehot_encoder_geo = pickle.load(f)
 
+with open("scaler.pkl", "rb") as f:
+    scaler = pickle.load(f)
 
-geography=st.selectbox('Geography',onehot_encoder_geo.categories[0])
-gender=st.selectbox('Gender',label_encoder_gender.classes_)
-age=st.slider('Age',18,92)
-balance=st.number_input('Balance')
-credit_score=st.number_input('Credit Score')
-estimated_salary=st.number_input('Estimated Salary')
-tenure=st.slider('Tenure',0,10)
-num_of_products=st.slider('Number Of Products',1,4)
-has_cr_card=st.selectbox('Has Credit Card',[0,1])
-is_active_member=st.selectbox('Is Active Member',[0,1])
+st.title("Customer Churn Prediction")
 
-input_data={
-    'CreditScore':600,
-    'Geography':'France',
-    'Gender':'Male',
-    'Age':40,
-    'Tenure':3,
-    'Balance':60000,
-    'NumOfProducts':2,
-    'HasCrCard':1,
-    'IsActiveMember':1,
-    'EstimatedSalary':50000
-}
+# Inputs
+geography = st.selectbox("Geography", onehot_encoder_geo.categories_[0])
+gender = st.selectbox("Gender", label_encoder_gender.classes_)
+age = st.slider("Age", 18, 92)
+credit_score = st.number_input("Credit Score", 300, 900)
+balance = st.number_input("Balance")
+estimated_salary = st.number_input("Estimated Salary")
+tenure = st.slider("Tenure", 0, 10)
+num_of_products = st.slider("Number Of Products", 1, 4)
+has_cr_card = st.selectbox("Has Credit Card", [0, 1])
+is_active_member = st.selectbox("Is Active Member", [0, 1])
+
+if st.button("Predict"):
+    input_data = {
+        "CreditScore": credit_score,
+        "Gender": label_encoder_gender.transform([gender])[0],
+        "Age": age,
+        "Tenure": tenure,
+        "Balance": balance,
+        "NumOfProducts": num_of_products,
+        "HasCrCard": has_cr_card,
+        "IsActiveMember": is_active_member,
+        "EstimatedSalary": estimated_salary
+    }
+
+    geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+    geo_df = pd.DataFrame(
+        geo_encoded,
+        columns=onehot_encoder_geo.get_feature_names_out(["Geography"])
+    )
+
+    input_df = pd.DataFrame([input_data])
+    final_df = pd.concat([input_df, geo_df], axis=1)
+
+    input_scaled = scaler.transform(final_df)
+    prediction = model.predict(input_scaled)[0][0]
+
+    if prediction > 0.5:
+        st.error("Customer will leave the bank")
+    else:
+        st.success("Customer will NOT leave the bank")
