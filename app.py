7235d6ac02164ebfd47a93c53838a37cbda2bc0b
Aditya Bajaj <aditya3105@users.noreply.huggingface.co>
2026-01-16 07:16:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pickle
+import pandas as pd
+import tensorflow as tf
+# from tensorflow.keras.models import load_model
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+import pickle
 
-"""
-# Welcome to Streamlit!
+model = tf.keras.model.load_model('model.h5')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+with open('label_encoder_gender.pkl','wb') as file:
+    label_encoder_gender=pickle.load(file)
+with open('onehot_encoder_geo.pkl','wb') as file:
+    onehot_encoder_geo=pickle.load(file)
+with open('scaler.pkl','rb') as file:
+    scaler=pickel.load(file)
+st.title('customer churn prediction')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+geography=st.selectbox('Geography',onehot_encoder_geo.categories[0])
+gender=st.selectbox('Gender',label_encoder_gender.classes_)
+age=st.slider('Age',18,92)
+balance=st.number_input('Balance')
+credit_score=st.number_input('Credit Score')
+estimated_salary=st.number_input('Estimated Salary')
+tenure=st.slider('Tenure',0,10)
+num_of_products=st.slider('Number Of Products',1,4)
+has_cr_card=st.selectbox('Has Credit Card',[0,1])
+is_active_member=st.selectbox('Is Active Member',[0,1])
